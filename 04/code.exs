1734603b109aa2b5dbb74550750a0d929a0f71ab
Francesco Donadon <francesco.donadon@gmail.com>
2019-12-09 19:45:52 +0100

    Day 04 part 2

--- 04/code.exs
+++ 04/code.exs
@@ -3,30 +3,26 @@ defmodule Password do
     Integer.to_charlist(pass)
   end
 
-  def has_double_digits?([first, second | rest]) do
-    if first == second do
-      true
-    else
-      has_double_digits?([second | rest])
-    end
+  def has_double_digits?(number) do
+    number
+    |> Enum.chunk_by(& &1)
+    |> Enum.any?(&length(&1) == 2)
   end
 
-  def has_double_digits?(_), do: false
-
   def has_ascending_digits?([first, second | rest]) do
-    unless first <= second do
+    if first > second do
       false
     else
       has_ascending_digits?([second | rest])
     end
   end
 
-  def has_ascending_digits?(u), do: IO.inspect(u)
+  def has_ascending_digits?(_), do: true
 end
 
 372304..847060
 |> Enum.map(&Password.convert/1)
-|> Enum.filter(&Password.has_double_digits?/1)
 |> Enum.filter(&Password.has_ascending_digits?/1)
+|> Enum.filter(&Password.has_double_digits?/1)
 |> length()
 |> IO.inspect
